e1cc99543576b31c4d7a0a7ce7db47049f5454b4
Ganesh Anand <gan3sh500@gmail.com>
2016-04-10 15:19:34 +0530

    Added ALS as function.

--- code.py
+++ code.py
@@ -51,6 +51,31 @@ def movieKMeans(itemDat):#to reduce the total number of movie features
     centroids,distortion = scipy.cluster.vq.kmeans(D,num_clusters)#
     idx,_=scipy.cluster.vq.vq(D,centroids)#each movie is in a cluster
     print idx
+#%%
+def ALS_algo(R,W,n_factors=8,lambda_=10,n_iterations=10):
+    Q=R
+    m, n = Q.shape
+    X = 5 * np.random.rand(m, n_factors) 
+    Y = 5 * np.random.rand(n_factors, n)
+
+    print "Starting iterations..."
+    for ii in range(n_iterations):
+        for u, Wu in enumerate(W):
+            X[u] = np.linalg.solve(np.dot(Y, np.dot(np.diag(Wu), Y.T)) + lambda_ * np.eye(n_factors),
+                                    np.dot(Y, np.dot(np.diag(Wu), Q[u].T))).T
+        for i, Wi in enumerate(W.T):
+            Y[:,i] = np.linalg.solve(np.dot(X.T, np.dot(np.diag(Wi), X)) + lambda_ * np.eye(n_factors),
+                                    np.dot(X.T, np.dot(np.diag(Wi), Q[:, i])))
+        print('{}th iteration is completed of {}'.format(ii + 1,n_iterations))
+        TEMP=get_error(Q, X, Y, W)
+        print('RMS Error on Training Set after {}th iteration is {}'.format(ii + 1,TEMP))
+        print('Saving current X and Y...')
+        #np.savetxt('X.txt', X);
+        #np.savetxt('Y.txt', Y);
+        print('Saved.')
+    weighted_Q_hat = np.dot(X,Y)
+    return weighted_Q_hat,X,Y
+#%%
 ############# IMPORTING THE DATASETS ###################
 
 os.chdir("/home/rsk/Documents/RecommenderProject/")
@@ -112,5 +137,3 @@ get_train_error(R,predictedR,W,rmse=True)
 #%%
 get_test_error( test,predictedR,rmse=True )
 #%%
-
-
