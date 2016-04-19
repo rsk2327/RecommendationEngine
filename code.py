1a4b0ba4a9153bcb337406a3ce5b7d319b86cb8b
Ganesh Anand <gan3sh500@gmail.com>
2016-04-20 03:02:55 +0530

    Ran createDataFrame
    Made modifications to code to make it run and save.

--- code.py
+++ code.py
@@ -11,16 +11,16 @@ import matplotlib.pyplot as plt
 from sklearn.cross_validation import train_test_split
 from scipy.sparse import csr_matrix
 import scipy.cluster.vq
-import nimfa as nf
+#import nimfa as nf
 
-def get_train_error( trueRating, predRating, W, rmse=False ):            #Computes error for the training dataset
+def get_train_error(trueRating, predRating, W, rmse=False):            #Computes error for the training dataset
     
     ss = np.sum( np.square(trueRating - np.multiply(predRating,W)) )
     if rmse==True:
         return np.sqrt(ss/np.sum(W))
     else:
         return ss
-
+#%%
 def get_test_error( testData, predRating, rmse=False ):
     
     ss=[]
@@ -35,26 +35,24 @@ def get_test_error( testData, predRating, rmse=False ):
     else:
         return ss
 #%%
-def createDataFrame(ratingsDat,itemDat,userDat,als):#,R): #to create DataFrame for randomForest
+def createDataFrame(ratingsDat,itemDat,userDat,predictedR): #to create DataFrame for randomForest
     D = pd.merge(ratingsDat,itemDat,on="movieID")
     D = pd.merge(D,userDat,on="userID")
-    D['ALS']=0
     D = D.drop(D.ix[:,['movie title','video release date','IMDb URL']].head(0).columns,axis=1)
-    #D['ALS'] = R[D.loc[:,'userID']-1 , D.loc[:,'movieID']-1]
-    D.to_csv('DataFrame.csv')        #saving
-    train,test=train_test_split(D,)
-    #alsfunc()
+    D['ALS'] = predictedR[D.loc[:,'userID']-1 , D.loc[:,'movieID']-1]
+    D.to_csv('combinedData.csv')        #saving
+    #train,test=train_test_split(D,)
     #print D
 #%%
-def movieKMeans():#to reduce the total number of movie features
+def movieKMeans(itemDat):#to reduce the total number of movie features
     #np.set_printoptions(threshold=10)
-    os.chdir("/home/satvik/Analytics/Recommender Project")
-    itemDat = pd.read_table("ml-100k/u.item",sep="|",header=None)
-    itemDat.columns = ['movieID', 'movie title' , 'release date' , 'video release date' ,
-              'IMDb URL' , 'unknown' , 'Action' , 'Adventure' , 'Animation' ,
-              'Childrens' , 'Comedy' , 'Crime' , 'Documentary' , 'Drama' , 'Fantasy' ,
-             ' Film-Noir' , 'Horror' , 'Musical' , 'Mystery' , 'Romance' , 'Sci-Fi' ,
-              'Thriller' , 'War' , 'Western']
+    #os.chdir("/home/satvik/Analytics/Recommender Project")
+    #itemDat = pd.read_table("ml-100k/u.item",sep="|",header=None)
+    #itemDat.columns = ['movieID', 'movie title' , 'release date' , 'video release date' ,
+    #          'IMDb URL' , 'unknown' , 'Action' , 'Adventure' , 'Animation' ,
+    #          'Childrens' , 'Comedy' , 'Crime' , 'Documentary' , 'Drama' , 'Fantasy' ,
+    #         ' Film-Noir' , 'Horror' , 'Musical' , 'Mystery' , 'Romance' , 'Sci-Fi' ,
+    #          'Thriller' , 'War' , 'Western']
     #D = scipy.cluster.vq.whiten(D)#feature-scale
     num_clusters =3
     D = itemDat.loc[:,'Action':'Western'].values
@@ -62,7 +60,6 @@ def movieKMeans():#to reduce the total number of movie features
     idx,_=scipy.cluster.vq.vq(D,centroids)#each movie is in a cluster
     #print idx
     return idx
-movieKMeans()
 #%%
 def ALS_algo(R,W,n_factors=8,lambda_=10,n_iterations=10):
     Q=R
@@ -79,18 +76,20 @@ def ALS_algo(R,W,n_factors=8,lambda_=10,n_iterations=10):
             Y[:,i] = np.linalg.solve(np.dot(X.T, np.dot(np.diag(Wi), X)) + lambda_ * np.eye(n_factors),
                                     np.dot(X.T, np.dot(np.diag(Wi), Q[:, i])))
         print('{}th iteration is completed of {}'.format(ii + 1,n_iterations))
-        TEMP=get_error(Q, X, Y, W)
-        print('RMS Error on Training Set after {}th iteration is {}'.format(ii + 1,TEMP))
-        print('Saving current X and Y...')
-        #np.savetxt('X.txt', X);
-        #np.savetxt('Y.txt', Y);
-        print('Saved.')
+        # TEMP=get_error(Q, X, Y, W)
+        # print('RMS Error on Training Set after {}th iteration is {}'.format(ii + 1,TEMP))
+        # print('Saving current X and Y...')
+        # np.savetxt('X.txt', X);
+        # np.savetxt('Y.txt', Y);
+        # print('Saved.')
     weighted_Q_hat = np.dot(X,Y)
     return weighted_Q_hat,X,Y
-#
-############## IMPORTING THE DATASETS ###################
+#%%
 
-os.chdir("/home/satvik/Analytics/Recommender Project")
+
+############## IMPORTING THE DATASETS ###################
+print ('Importing data...')
+os.chdir("C://Users/Ganesh Anand/Desktop/Recommendation Engine")
 ratingsDat = pd.read_table("ml-100k/u.data",sep="\t",header=None)
 ratingsDat.columns=['userID','movieID','rating','timestamp']
 ratingsDat['userID'] = ratingsDat['userID'].astype("category")            #converting into categorical variables
@@ -101,52 +100,58 @@ itemDat.columns = ['movieID', 'movie title' , 'release date' , 'video release da
               'IMDb URL' , 'unknown' , 'Action' , 'Adventure' , 'Animation' ,
               'Childrens' , 'Comedy' , 'Crime' , 'Documentary' , 'Drama' , 'Fantasy' ,
              ' Film-Noir' , 'Horror' , 'Musical' , 'Mystery' , 'Romance' , 'Sci-Fi' ,
-              'Thriller' , 'War' , 'Western']
-              
-# TO-DO : Convert the genres into categorical variables   
-              
+              'Thriller' , 'War' , 'Western']              
+# TO-DO : Convert the genres into categorical variables                 
 userDat = pd.read_table("ml-100k/u.user",sep="|",header=None)
 userDat.columns = ['userID','age','gender','occupation','zipcode']
+print ('Data Imported.')
 
+print ('Splitting data...')
 train,test = train_test_split(ratingsDat,test_size=0.2,random_state=1)
-#print ratingsDat
-#itemDat['cluster'] = movieKMeans(itemDat)
-#itemDat['cluster'] = itemDat['cluster'].astype('category')
-#movieKMeans(itemDat)
-#%%##########  CREATING RATINGS MATRIX ###########################
+print ('Split done.')
+# print ratingsDat
 
+print ('Clustering...')
+itemDat['cluster'] = movieKMeans(itemDat)
+itemDat['cluster'] = itemDat['cluster'].astype('category')
+print ('Done clustering.')
 
 num_users = len(ratingsDat['userID'].cat.categories)
 num_movies = len(ratingsDat['movieID'].cat.categories)
-
+#%%##########  CREATING RATINGS MATRIX ###########################
 #Creating ratings matrix R and weight matrix W. 
 # R : Ratings matrix (only has ratings from the train dataset)
 # W : Keep check of which cells have ratings
 
-
+print ('Making Ratings and Weight matrix...')
 R = np.zeros((num_users,num_movies))
 W = np.zeros((num_users,num_movies))  
 
 for i in range(len(train)):
     R[ train.iloc[i]['userID']-1 , train.iloc[i]['movieID']-1 ] = train.iloc[i]['rating']    #userID is 1-based while R matrix is 0-based
     W[ train.iloc[i]['userID']-1 , train.iloc[i]['movieID']-1 ] = 1
-    
-sparseR = csr_matrix(R)    
 
-#%%################################################################
+print ('Matrices created.')
 
-## ALS implementation using NIMFA package
-als =  nf.Lsnmf(sparseR,seed="random_vcol",rank=100,max_iter=15,beta=0.1)            #Try using different rank (#of features) and see the reduction in training error
-als_fit= als.factorize()
+print ('Running ALS...')
+predictedR,X,Y=ALS_algo(R,W,n_factors=8,lambda_=10,n_iterations=10)
+# sparseR = csr_matrix(R)    
+# #%%################################################################
+# ## ALS implementation using NIMFA package
+# als =  nf.Lsnmf(sparseR,seed="random_vcol",rank=100,max_iter=15,beta=0.1)            #Try using different rank (#of features) and see the reduction in training error
+# als_fit= als.factorize()
+# #%%
+# user_features = als_fit.basis()
+# movie_features = als_fit.coef()
+# print user_features.todense()[1:10][1:10]
+# predictedR = np.dot( user_features.todense() , movie_features.todense() )
+print ('ALS done.')
 
-
+train_err = get_train_error(R,predictedR,W,rmse=True)
 #%%
-user_features = als_fit.basis()
-movie_features = als_fit.coef()
-print user_features.todense()[1:10][1:10]
-predictedR = np.dot( user_features.todense() , movie_features.todense() )
+test_err = get_test_error(test,predictedR,rmse=True)
+#%%
 
-get_train_error(R,predictedR,W,rmse=True)
-#%%
-get_test_error( test,predictedR,rmse=True )
-#%%
+print ('Saving combined dataframe for random forest...')
+createDataFrame(ratingsDat,itemDat,userDat,predictedR)
+print ('Data saved.')
