fb8961997dd9d773fccd4b8cf6383e11fce6d044
Ganesh Anand <gan3sh500@gmail.com>
2016-04-20 16:14:31 +0530

    Working Randomforest with RMSE 0.8486
    ALS data was giving RMSE of 0.96

--- randomForest.py
+++ randomForest.py
@@ -1,37 +1,22 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
 import pandas as pd
+import numpy as np
 import pylab
 import os
+from sklearn.cross_validation import train_test_split
 
 # X.describe() shows all numerical variables
 # X.head shows all numerical and categorical variables
-
-# The Out of Box Score of with and without 
-# OOB score = 0.425379456052
-# OOB score = 0.427048611386
+# R2 score is 0.434810008711
+# RMSE score is 0.848644536894
 
 print ('Importing data...')
-os.chdir("C://Users/Ganesh Anand/Desktop/Recommendation Engine")
 X = pd.read_csv("combinedData.csv")
-X.describe()
 #After checking the columns I found that the indexes for ratings were showing up here
 X = X.drop(X.columns[0],axis=1)
-#Removed release dates as I was unsure how to use it
 X = X.drop(['release date','zipcode'],axis=1)
-y = X.pop('rating')
 print ('Data imported.')
-
-numeric_variables = list(X.dtypes[X.dtypes != "object"].index)
-X[numeric_variables].head()
-#I have 2 categorical variables left occupation and gender
-
-print('Creating Random Forest and running fit method...')
-model = RandomForestRegressor(n_estimators=100, oob_score=True, random_state=42)
-model.fit(X[numeric_variables],y)
-print('Tasks completed.')
-print('Initial benchmark done.')
-print('OOB score = {}'.format(model.oob_score_))
-#Initial model done.
 
 categorical_variables = ["gender", "occupation"]
 for variable in categorical_variables:
@@ -40,14 +25,22 @@ for variable in categorical_variables:
     X = pd.concat([X, dummies], axis=1)
     X.drop([variable], axis=1, inplace=True)
 
-print('Done building variables.\nBuilding model...')
-model = RandomForestRegressor(100, oob_score=True, random_state=42)
-model.fit(X,y)
-print('Second model built and benchmark done.')
-print('OOB score = {}'.format(model.oob_score_))
+train,test = train_test_split(X,test_size=0.2,random_state=1)
+ytrain = train.pop('rating')
+ytest = test.pop('rating')
 
-feature_importances = pd.Series(model.feature_importances_,index=X.columns)
+print('Done building variables.\nBuilding model...')
+model = RandomForestRegressor(100, oob_score=True, random_state=42, n_jobs=-1)
+model.fit(train,ytrain)
+print('Model built.\nRunning benchmarks...')
+r2 = r2_score(ytest, model.predict(test))
+rmse = np.sqrt(np.mean((ytest - model.predict(test))**2))
+
+feature_importances = pd.Series(model.feature_importances_,index=train.columns)
 feature_importances.sort(ascending=False)
 few_features = feature_importances[0:12]
 few_features.plot(kind="barh",figsize=(7,6))
 pylab.show()
+
+print('R2 score is {}'.format(r2))
+print('RMSE score is {}'.format(rmse))
