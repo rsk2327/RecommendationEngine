f856bfafdc14a7f51be53597ddb8ba5dd8957857
Ganesh Anand <gan3sh500@gmail.com>
2016-06-17 00:59:01 +0530

    Added ALS to utils

--- Cleaned-incomplete/utils.py
+++ Cleaned-incomplete/utils.py
@@ -2,37 +2,30 @@ import datetime
 import pandas as pd
 import numpy as np
 
-def import100k(path = '/', test_size = 0.2, random_state = 1):
-	# Function that imports MovieLens 100k data from given path 
-	import os
-	import pandas as pd
-	import numpy as np
-	from scipy.sparse import csr_matrix
-	from sklearn.cross_validation import train_test_split
-	outfile_test = 'test_mat.npy'
-	outfile_train = 'train_mat.npy'
-	print('Reimporting data...')
-	os.chdir(path)
-	ratingsDat = pd.read_table("ml-100k/u.data",sep="\t",header=None)
-	ratingsDat.columns = ['userID','movieID','rating','timestamp']
-	ratingsDat['userID'] = ratingsDat['userID'].astype("category")
-	ratingsDat['movieID'] = ratingsDat['movieID'].astype('category')
-	train,test = train_test_split(ratingsDat,test_size=test_size,random_state=random_state)
-	n_u = len(ratingsDat['userID'].cat.categories)
-	n_m = len(ratingsDat['movieID'].cat.categories)
-	test_col = np.array(test['userID'].values)-1
-	test_row = np.array(test['movieID'].values)-1
-	test_dat = np.array(test['rating'].values)
-	train_col = np.array(train['userID'].values)-1
-	train_row = np.array(train['movieID'].values)-1
-	train_dat = np.array(train['rating'].values)
-	train_mat = (csr_matrix((train_dat, (train_row, train_col)), shape=(n_m, n_u)).toarray()).T
-	test_mat = (csr_matrix((test_dat, (test_row, test_col)), shape=(n_m, n_u)).toarray()).T
-	np.save(outfile_test, test_mat)
-	np.save(outfile_train, train_mat)
-	return train_mat, test_mat
+def als(train):
+	n_factors = 8
+	n_iterations = 15
+	lambda_ = 10
+	m, n = train.shape
+	Q = train
+	W = Q > 0.5
+	W = W.astype(int)
+	print('X and Y randomly initialzied.')
+	X = 5 * np.random.rand(m, n_factors) 
+	Y = 5 * np.random.rand(n_factors, n)
+	for ii in range(n_iterations):
+		for u, Wu in enumerate(W):
+			X[u] = np.linalg.solve(np.dot(Y, np.dot(np.diag(Wu), Y.T)) + lambda_ * np.eye(n_factors),
+	                                np.dot(Y, np.dot(np.diag(Wu), Q[u].T))).T
+		for i, Wi in enumerate(W.T):
+			Y[:,i] = np.linalg.solve(np.dot(X.T, np.dot(np.diag(Wi), X)) + lambda_ * np.eye(n_factors),
+	                                np.dot(X.T, np.dot(np.diag(Wi), Q[:, i])))
+		print('{}th iteration is completed of {}'.format(ii + 1,n_iterations))
+	prediction = np.dot(X,Y)
+	print('Done.')
+	return prediction, X, Y
 
-def importData(dataDir):
+def importData(dataDir=''):
     """
     Imports u.data, u.item and u.user and returns it in DataFrame format
     as ratingData, movieData and userData
@@ -59,21 +52,18 @@ def cleanData(data):
     Pre-process the data. Variable type changes, missing value removal etc.
     """
     data = data.drop(data.index[[2172, 3781, 7245, 12475, 14756, 15292, 49295, 93523, 99723]],axis=0,inplace=False)
-    data = data.drop("video release date",axis=1)
+    data = data.drop(["video release date", 'IMDb URL'],axis=1)
     
     data.userID = data.userID.astype("category")
     data.movieID = data.movieID.astype("category")
     data.gender = data.gender.astype("category")
     data.occupation = data.occupation.astype("category")
     
-    
-#    data = dateTime(data)
+	#data = dateTime(data)
     
     return data
 
 def dateTime(data):
-    
-    
     val = data.timestamp.apply(datetime.datetime.fromtimestamp)
     
     data['year'] = val.apply(lambda x : x.year)
@@ -95,34 +85,7 @@ def strpReleaseDate(x):
 def RMSE(y_pred,y_true):
     return np.sqrt(np.mean((y_true - y_pred)**2))
 
-def prepareForRF(path = '/', prediction=False, X=False, Y=False):
-	outfile_combined = 'combinedData.csv'
-	remport = True
-	if op.isfile(outfile_combined):
-		reimport = False
-		data = pd.read_csv(outfile_combined)
-	if remport:	
-		ratingsDat = pd.read_table("ml-100k/u.data",sep="\t",header=None)
-		ratingsDat.columns=['userID','movieID','rating','timestamp']
-		ratingsDat['userID'] = ratingsDat['userID'].astype("category")
-		ratingsDat['movieID'] = ratingsDat['movieID'].astype('category')
-
-		itemDat = pd.read_table("ml-100k/u.item",sep="|",header=None)
-		itemDat.columns = ['movieID', 'movie title' , 'release date' , 'video release date' ,
-              'IMDb URL' , 'unknown' , 'Action' , 'Adventure' , 'Animation' ,
-              'Childrens' , 'Comedy' , 'Crime' , 'Documentary' , 'Drama' , 'Fantasy' ,
-             ' Film-Noir' , 'Horror' , 'Musical' , 'Mystery' , 'Romance' , 'Sci-Fi' ,
-              'Thriller' , 'War' , 'Western']              
-		userDat = pd.read_table("ml-100k/u.user",sep="|",header=None)
-		userDat.columns = ['userID','age','gender','occupation','zipcode']
-		data = pd.merge(ratingsDat, itemDat, on='movieID')
-		data = pd.merge(data, userDat, on='userID')
-		data = data.drop(data.ix[:,['movie title','video release date','IMDb URL']].head(0).columns,axis=1)
-		if not prediction and not X and not Y:
-			data['ALS'] = prediction[data.loc[:, 'userID']-1, data.loc[:, 'movieID']-1]
-			for i in range(len(Y)):
-				data["UserFeature{}".format(i)] = X[data.loc[:, 'userID']-1, i]
-				data["MovieFeature{}".format(i)] = Y[i, data.loc[:, 'movieID']-1]
-		data.to_csv(outfile_combined)
-	return data
-    #%%
+def RMSE_matrix(prediction, test_mat):
+	mask = test_mat>0;	mask[mask==True]=1; mask[mask==False]=0
+	prediction = np.multiply(prediction, mask)
+	return (np.sum((prediction - test_mat)**2)/np.sum(mask))**0.5
